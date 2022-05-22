f07ef755ac1605a186e9f2385b3167b23dc3d1f0
richharv557 <richharv557@gmail.com>
2022-05-22 18:01:05 -0500

    Added random CPU behavior, need to fix issue

--- mastermind.rb
+++ mastermind.rb
@@ -20,22 +20,35 @@ class Mastermind
     puts 'Welcome to MASTERMIND. The game of Mind Masters.'
     puts ''
     puts "MASTERMIND is a game where you have to guess your opponent's secret code within a limited number of guesses."
-    puts 'For our game, the code will be 4 numeric digits (1-6) and you have 12 guesses to crack the code'
+    puts 'For our game, the code will be 4 numeric digits (1-6) and the breaker has 12 guesses to crack the code'
+    puts 'Press 1 if you would like to be BREAKER or 2 if you would like to be MAKER. The CPU will be your oponent if you are MAKER.'
+  end
+
+  def select_mode
+    input = gets.chomp.to_i
+    until input == 1 || input == 2
+      puts 'Invalid input. Press 1 if you would like to be BREAKER or 2 if you would like to be MAKER.'
+      input = gets.chomp.to_i
+    end
+    input
   end
 
   def play_game
     welcome
-    # choose mode to be added
-    breaker = Player.new
-    select_random_code
+    if select_mode == 1
+      breaker = Player.new
+      select_random_code
+    else
+      breaker = CPU.new
+    end
     play_round(breaker) until @game_over
     print_game_over_message
   end
 
   def play_round(breaker)
     breaker.capture_input
-    check_if_partial_match(breaker.guessed_code)
-    check_if_exact_match(breaker.guessed_code)
+    check_if_partial_match(breaker.input_code)
+    check_if_exact_match(breaker.input_code)
     create_hint
     print_hint_and_round_info
     check_for_win
@@ -43,6 +56,10 @@ class Mastermind
   end
 
   def select_random_code
+    4.times { @code_to_solve.push(rand(1..6)) }
+  end
+
+  def select_code
     4.times { @code_to_solve.push(rand(1..6)) }
   end
 
@@ -98,10 +115,10 @@ end
 
 # Player class represents the player and the computer playing the game
 class Player
-  attr_reader :guessed_code
+  attr_reader :input_code
 
   def initialize
-    @guessed_code = []
+    @input_code = []
   end
 
   def validate_input(code)
@@ -115,7 +132,15 @@ class Player
       puts 'Invalid input. Please enter a 4 digit code using 1-6 for each digit.'
       input = gets.chomp.to_i
     end
-    @guessed_code = input.to_s.split('').map(&:to_i)
+    @input_code = input.to_s.split('').map(&:to_i)
+  end
+end
+
+# CPU class replaces the user input with Random guesses and then ultimately algorithmically guided guesses
+class CPU < Player
+  def capture_input
+    @input_code = []
+    4.times { @input_code.push(rand(1..6)) }
   end
 end
 
