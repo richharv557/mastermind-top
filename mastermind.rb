a43a61159be63a12569b8db75fbb127790f93499
richharv557 <richharv557@gmail.com>
2022-05-21 19:08:09 -0500

    Some cleanup, working on hint creation

--- mastermind.rb
+++ mastermind.rb
@@ -30,19 +30,22 @@ class Mastermind
     breaker = Player.new
     breaker.capture_input
     select_random_code
-    check_if_input_included(@guessed_code)
-    check_if_input_included_in_right_spot(@guessed_code)
+    check_if_input_included(breaker.guessed_code)
+    check_if_input_included_in_right_spot(breaker.guessed_code)
     p create_hint
   end
 
   def select_random_code
-    4.times { @code_to_solve.push(rand(1..6)) }
+    # 4.times { @code_to_solve.push(rand(1..6)) }
+    # 4.times { @code_to_solve.push(1) }
+    @code_to_solve = [4, 3, 2, 1]
   end
 
   # first split the input code into an array and see if the solution contains the character
+  # This is flawed, i don't think it handles the logic for guessing 1 number where it appears multiple times, will test
   def check_if_input_included(input)
     @includes_counter = 0
-    input_array = input.split('').map(&:to_i)
+    input_array = input.to_s.split('').map(&:to_i)
     input_array.each do |guess_number|
       @includes_counter += 1 if @code_to_solve.include?(guess_number)
     end
@@ -53,7 +56,7 @@ class Mastermind
     @includes_and_location_counter = 0
     input_hash = {}
     code_to_solve_hash = {}
-    input.split('').map(&:to_i).each_with_index { |number, index| input_hash[index + 1] = number }
+    input.to_s.split('').map(&:to_i).each_with_index { |number, index| input_hash[index + 1] = number }
     @code_to_solve.each_with_index { |number, index| code_to_solve_hash[index + 1] = number }
     4.times do |i|
       @includes_and_location_counter += 1 if input_hash[i + 1] == code_to_solve_hash[i + 1]
@@ -62,8 +65,9 @@ class Mastermind
 
   def create_hint
     @hint = ''
-    @includes_and_location_counter.times { @hint = "#{hint}+" }
-    (@includes_counter - @includes_and_location_counter).times { @hint = "#{hint}-" }
+    @includes_and_location_counter.times { @hint += '+' }
+    (@includes_counter - @includes_and_location_counter).times { @hint += '-' }
+    @hint
   end
 
   def check_for_win
@@ -75,15 +79,22 @@ end
 
 # Player class represents the player and the computer playing the game
 class Player
-  # need to add while loop to ensure valid input
+  attr_reader :guessed_code
+
+  def initialize
+    @guessed_code = 0
+  end
+
   def validate_input(code)
     code.is_a?(Integer) && code >= 1111 && code <= 6666
   end
 
   def capture_input
-    @guessed_code = gets 'Please enter a 4 digit code using 1-6 for each digit.'
+    puts 'Please enter a 4 digit code using 1-6 for each digit.'
+    @guessed_code = gets.chomp.to_i
     until validate_input(@guessed_code)
-      @guessed_code = gets 'Invalid input. Please enter a 4 digit code using 1-6 for each digit.'
+      puts 'Invalid input. Please enter a 4 digit code using 1-6 for each digit.'
+      @guessed_code = gets.chomp.to_i
     end
   end
 end
