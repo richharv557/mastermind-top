01a823e8fd5e641b9c2579ff13281b36960cb76c
richharv557 <richharv557@gmail.com>
2022-05-22 22:50:27 -0500

    Added CPU guessing algorithm

--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 # MasterMind class currently contains game logic and state
+# Maybe need to split the class apart from the game outside of rounds, currently bugging
 class Mastermind
-  attr_reader :code_to_solve, :includes_and_location_counter, :includes_counter
+  attr_reader :code_to_solve, :includes_and_location_counter, :includes_counter, :remaining_codes
 
   def initialize
     @code_to_solve = []
-    @guessed_code = []
     @hint = ''
     @round = 1
     @game_over = false
+    @keep_playing = true
     @mode = 0
+    @remaining_codes = [1, 2, 3, 4, 5, 6].repeated_permutation(4).to_a
   end
 
   def welcome
@@ -22,13 +24,13 @@ class Mastermind
     puts ''
     puts "MASTERMIND is a game where you have to guess your opponent's secret code within a limited number of guesses."
     puts 'For our game, the code will be 4 numeric digits (1-6) and the breaker has 12 guesses to crack the code'
-    puts 'Press 1 if you would like to be BREAKER or 2 if you would like to be MAKER. The CPU will be your oponent if you are MAKER.'
+    puts 'Enter 1 to be code-BREAKER or 2 for code-MAKER. The CPU will be your oponent if you are MAKER.'
   end
 
   def select_mode
     input = gets.chomp.to_i
     until input == 1 || input == 2
-      puts 'Invalid input. Press 1 if you would like to be BREAKER or 2 if you would like to be MAKER.'
+      puts 'Invalid input. Enter 1 if you would like to be BREAKER or 2 if you would like to be MAKER.'
       input = gets.chomp.to_i
     end
     @mode = input
@@ -36,33 +38,38 @@ class Mastermind
   end
 
   def play_game
-    welcome
-    if select_mode == 1
-      breaker = Player.new
-      select_random_code
-    else
-      breaker = CPU.new
-      @code_to_solve = breaker.capture_starting_guess
+    while @keep_playing
+      welcome
+      if select_mode == 1
+        breaker = Player.new
+        select_random_code
+      else
+        breaker = CPU.new
+        @code_to_solve = breaker.capture_starting_guess
+      end
+      play_round(breaker) until @game_over
+      print_game_over_message
+      ask_to_play_again
     end
-    play_round(breaker) until @game_over
-    print_game_over_message
   end
 
   def play_round(breaker)
-    breaker.capture_input
-    check_if_partial_match(breaker.input_code)
+    breaker.capture_input(self)
     check_if_exact_match(breaker.input_code)
+    check_if_partial_match(breaker.input_code)
     create_hint
     print_hint_and_round_info
     check_for_win
     check_for_loss
+    remove_possibilities if @mode == 2
+  end
+
+  def ask_to_play_again
+    puts "Do you want to play again? Enter 'y' to play again or anything else to exit."
+    @keep_playing = false unless gets.chomp == 'y'
   end
 
   def select_random_code
-    4.times { @code_to_solve.push(rand(1..6)) }
-  end
-
-  def select_code
     4.times { @code_to_solve.push(rand(1..6)) }
   end
 
@@ -71,7 +78,7 @@ class Mastermind
     input_hash = {}
     filtered_array = input_array.select { |number| @code_to_solve.include?(number) }
     filtered_array.each { |number| input_hash[number] = [@code_to_solve.count(number), input_array.count(number)].min }
-    @partial_matches = input_hash.values.sum
+    @partial_matches = input_hash.values.sum - @exact_matches
   end
 
   # split the input code into an hash and see if the sol contains char in loc (using k as location, v as guess number)
@@ -89,8 +96,18 @@ class Mastermind
   def create_hint
     @hint = ''
     @exact_matches.times { @hint += '+' }
-    (@partial_matches - @exact_matches).times { @hint += '-' }
-    @hint
+    @partial_matches.times { @hint += '-' }
+  end
+
+  def remove_possibilities
+    original_partials = @partial_matches
+    original_exacts = @exact_matches
+    temp_array = @remaining_codes.filter do |set|
+      check_if_partial_match(set)
+      check_if_exact_match(set)
+      @partial_matches + @exact_matches > original_partials + original_exacts
+    end
+    @remaining_codes = temp_array
   end
 
   def print_hint_and_round_info
@@ -108,7 +125,9 @@ class Mastermind
 
   def print_game_over_message
     puts 'Game over!'
-    if @hint == '++++'
+    if @hint == '++++' && @mode == 1
+      puts 'You Win!'
+    elsif @round > 12 && @mode == 2
       puts 'You Win!'
     else
       puts 'You Lose!'
@@ -128,7 +147,7 @@ class Player
     code.is_a?(Integer) && code >= 1111 && code <= 6666
   end
 
-  def capture_input
+  def capture_input(_object)
     puts 'Please enter a 4 digit code using 1-6 for each digit.'
     input = gets.chomp.to_i
     until validate_input(input)
@@ -137,15 +156,14 @@ class Player
     end
     @input_code = input.to_s.split('').map(&:to_i)
   end
-
-
 end
 
 # CPU class replaces the user input with Random guesses and then ultimately algorithmically guided guesses
+# Algorithm - randomly guess a code, then remove those codes that cannot match from the remaining ones
 class CPU < Player
-  def capture_input
+  def capture_input(object)
     @input_code = []
-    4.times { @input_code.push(rand(1..6)) }
+    @input_code = object.remaining_codes.sample
   end
 
   def capture_starting_guess
